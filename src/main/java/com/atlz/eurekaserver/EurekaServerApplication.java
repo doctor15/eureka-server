7fd1bbd44beba80de5eb40eaf01728b7585e5b49
yan <yan>
2017-12-06 10:51:03 +0800

    1.初始提交

---
+++ src/main/java/com/atlz/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.atlz.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka  基础服务，服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
